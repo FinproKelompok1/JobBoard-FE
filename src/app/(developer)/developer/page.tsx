aae86d1afbe7841aebff8c61c37b9aa5ada83cdf
muhammadwildansapoetro <muhwilsap@gmail.com>
2025-01-26 13:16:51 +0700

    developer dashboard page

--- src/app/(developer)/developer/page.tsx
+++ src/app/(developer)/developer/page.tsx
@@ -1,7 +1,30 @@
-export default function DeveloperPage(){
+export default function DeveloperPage() {
   return (
-    <main>
-      <h1>DEVELOPER PAGE</h1>
+    <main className="py-10">
+      <section className="container mx-auto">
+        <h1 className="font-bold text-3xl">Developer Dashboard</h1>
+      </section>
+
+      {/* Container for sidebar and main content */}
+      <section className="container mx-auto flex gap-5 mt-10">
+        {/* Sidebar */}
+        <aside className="w-1/5 border border-gray-300 p-5 rounded-lg">
+          <div className="flex flex-col gap-5 text-gray-500">
+            <ol className="font-bold">Subscription</ol>
+            <li className="list-none">Create Subscription</li>
+            <li className="list-none">Edit Subscription</li>
+            <li className="list-none">See Subscription</li>
+            <ol className="font-bold text-gray-500"> Assessment </ol>
+            <li className="list-none">Create Assessment</li>
+            <li className="list-none">Assessment Result</li>
+          </div>
+        </aside>
+
+        {/* Main Content */}
+        <section className="flex-1 border border-gray-300 p-5 rounded-lg">
+          <div>Main section</div>
+        </section>
+      </section>
     </main>
-  )
+  );
 }
